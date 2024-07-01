400abcd395b02bdb42056ab751640087c305a4ff
Parth Gupta <parth.eng1210@gmail.com>
2024-07-01 19:00:50 +0530

    feat: added thetre ticket contract

---
+++ contracts/ThetreTicket.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract ThetreTicket is 
+    Initializable, 
+    ERC721Upgradeable, 
+    ERC721EnumerableUpgradeable, 
+    ERC721URIStorageUpgradeable, 
+    PausableUpgradeable, 
+    OwnableUpgradeable, 
+    ERC721BurnableUpgradeable 
+{
+    uint256 private _tokenIdCounter;
+    
+    string private _baseTokenURI;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory name, string memory symbol, string memory baseTokenURI, uint256 maxTokenSupply, uint256 tokenPrice, address ownerAddress) initializer public {
+        __ERC721_init(name, symbol);
+        __ERC721Enumerable_init();
+        __ERC721URIStorage_init();
+        __Pausable_init();
+        __Ownable_init(ownerAddress);
+        __ERC721Burnable_init();
+
+        _baseTokenURI = baseTokenURI;
+    }
+
+
+    function safeMint() public payable {
+        uint256 tokenId = _tokenIdCounter;
+
+        _tokenIdCounter+=1;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function ownedTokens(address _owner) public view returns(uint256[] memory ownerTokens) {
+        uint256 tokenCount = balanceOf(_owner);
+
+        if (tokenCount == 0) {
+            // Return an empty array
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 resultIndex = 0;
+
+            uint256 index;
+            for (index = 0; index < tokenCount; index++) {
+                result[resultIndex] = tokenOfOwnerByIndex(_owner ,index);
+                resultIndex++;
+            }
+            return result;
+        }
+    }
+
+    function setBaseURI(string memory newBaseTokenURI) public onlyOwner {
+        _baseTokenURI = newBaseTokenURI;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // Overrides
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function baseURI() public view returns (string memory) {
+        return _baseTokenURI;
+    }
+}
