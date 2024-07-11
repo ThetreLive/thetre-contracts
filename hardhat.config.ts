0f81a8cff1480206c2152ca645235287ec6175eb
Parth Gupta <parth.eng1210@gmail.com>
2024-07-11 12:10:17 +0530

    chore: added theta network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import { vars } from "hardhat/config";
+import '@openzeppelin/hardhat-upgrades';
+import "@nomicfoundation/hardhat-chai-matchers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    theta: {
+      url: "https://eth-rpc-api-testnet.thetatoken.org/rpc",
+      accounts: [vars.get("PRIVATE_KEY")],
+    },
+    hardhat: {
+      forking: {
+        url: "https://eth-rpc-api-testnet.thetatoken.org/rpc",
+      }
+    }
+  },
+  
+  
 };
 
 export default config;
